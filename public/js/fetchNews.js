cac5957bcefcbca494e2c6031c562317dc16ae81
Rachel Nolan <rachel.nolan@atos.net>
2015-11-01 10:03:35 +0000

    pulls through news data when tab selected

--- public/js/fetchNews.js
+++ public/js/fetchNews.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   var apiKey = "9wur7sdh84azzazdt3ye54k4";
   var guardianAPI = "https://content.guardianapis.com/search?api-key=" + apiKey;
-  var guardianOptions = {
+  var guardianInitialOptions = {
     section: "uk-news",
     "show-fields": "trailText",
     format: "json"
@@ -22,5 +22,32 @@ $(document).ready(function() {
     $('#uk-news-feed').html(newsFeedHTML);
   };
 
-  $.getJSON(guardianAPI, guardianOptions, initialData);
+  $.getJSON(guardianAPI, guardianInitialOptions, initialData);
+
+
+  $(".news-tab").click(function() {
+    var sectionSelected = event.target.id;
+    var sectionContentEl = "#" + sectionSelected + "-feed";
+    var guardianUpdateOptions = {
+      "section": sectionSelected,
+      "show-fields": "trailText",
+      "format": "json"
+    };
+    function updateTabData(data) {
+      var newsResults = data.response.results
+      var newsFeedHTML = "";
+
+      $.each(newsResults.slice(0,5), function(i, item) {
+          newsFeedHTML +=   '<li> <a href="' 
+                            + item.webURL + '">' 
+                            + item.webTitle + '</a><p>' 
+                            + item.fields.trailText + '</p></li>';
+      });
+
+      $(sectionContentEl).html(newsFeedHTML);
+    };
+
+    console.log(event.target.id);
+    $.getJSON(guardianAPI, guardianUpdateOptions, updateTabData);
+  })
 })
